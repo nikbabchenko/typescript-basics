5fe88d6096048bdc66d0bc5ea1a9e501fcb7169b
mbabchenko <mbabchenko@host-192-168-0-103.soho.net.ua>
2019-10-05 15:08:49 +0300

    Adds read only

--- main.ts
+++ main.ts
@@ -1,5 +1,19 @@
 // Complex types Interface and type alias
 
+// Duck checking
+interface LabeledValue {
+  label: string;
+}
+
+function printLabel(labeledObj: LabeledValue) {
+  console.log(labeledObj.label);
+}
+
+let myObj = {size: 10, label: "Size 10 Object"};
+printLabel(myObj);
+
+// Types vs Interfaces
+
 interface ICar {
   model: string;
   brand: string;
@@ -17,3 +31,37 @@ type CarType = {
 class Car implements ICar {
   constructor(public model: string, public brand: string) {}
 }
+
+// Optional Parameters
+interface SquareConfig {
+  color?: string;
+  width?: number;
+}
+
+type Square = {
+  color: string;
+  area: number;
+};
+
+function createSquare(config: SquareConfig): Square {
+  let newSquare = {color: "white", area: 100};
+  if (config.color) {
+      newSquare.color = config.color;
+  }
+  if (config.width) {
+      newSquare.area = config.width * config.width;
+  }
+  return newSquare;
+}
+
+let mySquare = createSquare({color: "black"});
+
+
+// Read Only
+interface Point {
+  readonly x: number;
+  readonly y: number;
+}
+
+let p1: Point = { x: 10, y: 20 };
+p1.x = 5; // error!
