7dbe3a4436566e3efea8a71e8837850234bc330a
mbabchenko <mbabchenko@host-192-168-0-104.soho.net.ua>
2019-10-15 21:36:12 +0300

    Adds property decorator

--- main.ts
+++ main.ts
@@ -1,42 +1,29 @@
-/**
- * Class method decorator LoadingCount
- * 1. Adds loadingCount property to Class
- * 2. Decorates  Method with returns any promise and adds counter at starting call,
- * and decreases loadingCount value if promise failed/fullfilled
- *
- */
-function LoadingCount() {
-  return (proto: any, _methodName: string, descriptor: PropertyDescriptor) => {
-    // safety check if Class doesn't have loadingCount prop
-    if (!proto.loadingCount) {
-      proto.loadingCount = 0;
-    }
-
-    const originalMethod = descriptor.value;
-
-    descriptor.value = async function(...args: any[]) {
-      proto.loadingCount++;
-      return originalMethod
-        .apply(this, args)
-        .then(data => {
-          proto.loadingCount--;
-          return data;
-        })
-        .catch(err => {
-          proto.loadingCount--;
-          throw err;
-        });
-    };
-
-    return descriptor;
-  };
+export class IceCreamComponent {
+  @Emoji()
+  flavor = 'vanilla';
 }
 
 
-class Service {
-    // target === Service.prototype
-    // propertyName === "someMethod"
-    // propertyDesciptor === Object.getOwnPropertyDescriptor(Service.prototype, "someMethod")
-  @LoadingCount()
-  someMethod() {}
+// Property Decorator
+function Emoji() {
+  return function(target: Object, key: string | symbol) {
+
+    let val = target[key];
+
+    const getter = () =>  {
+        return val;
+    };
+    const setter = (next) => {
+        console.log('updating flavor...');
+        val = `🍦 ${next} 🍦`;
+    };
+
+    Object.defineProperty(target, key, {
+      get: getter,
+      set: setter,
+      enumerable: true,
+      configurable: true,
+    });
+
+  };
 }
