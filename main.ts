bc4613ace5140afbf9cb1ddd548b071c0007ba8c
mbabchenko <mbabchenko@ip-172-23-109-28.us-west-2.compute.internal>
2019-10-16 10:42:22 +0300

    adds simple class decorator

--- main.ts
+++ main.ts
@@ -1,42 +1,13 @@
-/**
- * Class method decorator LoadingCount
- * 1. Adds loadingCount property to Class
- * 2. Decorates  Method with returns any promise and adds counter at starting call,
- * and decreases loadingCount value if promise failed/fullfilled
- *
- */
-function LoadingCount() {
-  return (proto: any, _methodName: string, descriptor: PropertyDescriptor) => {
-    // safety check if Class doesn't have loadingCount prop
-    if (!proto.loadingCount) {
-      proto.loadingCount = 0;
-    }
 
-    const originalMethod = descriptor.value;
-
-    descriptor.value = async function(...args: any[]) {
-      proto.loadingCount++;
-      return originalMethod
-        .apply(this, args)
-        .then(data => {
-          proto.loadingCount--;
-          return data;
-        })
-        .catch(err => {
-          proto.loadingCount--;
-          throw err;
-        });
-    };
-
-    return descriptor;
-  };
+function AutomaticTransmittion(constr: Function) {
+  constr.prototype.automatic = true;
 }
 
 
-class Service {
-    // target === Service.prototype
-    // propertyName === "someMethod"
-    // propertyDesciptor === Object.getOwnPropertyDescriptor(Service.prototype, "someMethod")
-  @LoadingCount()
-  someMethod() {}
+@AutomaticTransmittion
+class Car {
 }
+
+const bmw = new Car();
+
+console.log(bmw['automatic']);
